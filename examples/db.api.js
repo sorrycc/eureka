2ecd9bcce8b7d87d18e6e66c73e97fad41b59b71
wondger <wondger@gmail.com>
2013-03-31 23:55:44 +0800

    db module api example

---
+++ examples/db.api.js
@@ -0,0 +1,78 @@
+/*
+ * @name: .js
+ * @description: 
+ * @author: wondger@gmail.com
+ * @date: 2013-03-31
+ * @param: 
+ * @todo: 
+ * @changelog: 
+ */
+
+var db = require("../db");
+
+/*
+ *db.put({
+ *    collection: "tag",
+ *    doc: {
+ *        name: "test" + new Date().getTime()
+ *    },
+ *    complete: function(err, doc) {
+ *        if (err) {
+ *            console.log(err.message);
+ *            return;
+ *        }
+ *
+ *        console.log(doc);
+ *    }
+ *});
+ */
+
+
+/*
+ *db.post({
+ *    collection: "tag",
+ *    doc: {
+ *        name: new Date().getTime()
+ *    },
+ *    options: {
+ *        //multi: true
+ *    },
+ *    complete: function(err, numAffected) {
+ *        if (err) {
+ *            console.log(err.message);
+ *            return;
+ *        }
+ *
+ *        console.log(numAffected);
+ *    }
+ *});
+ */
+
+
+/*
+ *db.del({
+ *    query: {
+ *        name: "1364741673597"
+ *    },
+ *    collection: "tag",
+ *    complete: function(err, numAffected) {
+ *        if (err) {
+ *            console.log(err.message);
+ *            return;
+ *        }
+ *
+ *        console.log(numAffected);
+ *    }
+ *});
+ */
+
+
+/*
+ *db.get({
+ *    collection: "tag",
+ *    complete: function(err, docs) {
+ *        console.log(docs);
+ *    }
+ *});
+ */
+
