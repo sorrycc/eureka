aa00be9ca0e5cca08e4d5dc4363d1fa90765bba6
shuier <zhudan000@gmail.com>
2013-04-23 21:42:40 +0800

    session list update

--- public/js/session/list.js
+++ public/js/session/list.js
@@ -8,6 +8,7 @@
  * @changelog: 
  */
 KISSY.add("session/list", function(S, Ajax, XTemplate) {
+    var D = S.DOM, E = S.Event;
     var List = function(opt) {
         if (!(this instanceof List)) return new List(opt);
 
@@ -22,6 +23,7 @@ KISSY.add("session/list", function(S, Ajax, XTemplate) {
             if (!this.el || !this.tpl) return;
 
             this.id = this.el.attr("data-id");
+            this.bind();
         },
         render: function() {
             var self = this;
@@ -36,7 +38,11 @@ KISSY.add("session/list", function(S, Ajax, XTemplate) {
                         return;
                     }
 
-                    self.el.html(new XTemplate(self.tpl).render((d.party)[0]));
+                    var partyInfo = (d.party)[0]
+                    var date = new Date(partyInfo.time);
+                    partyInfo.time = date.getFullYear() + '年' + (date.getMonth()+1) + '月' + date.getDate() + '日'
+
+                    self.el.html(new XTemplate(self.tpl).render(partyInfo));
 
                     self.bind();
                 }
@@ -44,18 +50,46 @@ KISSY.add("session/list", function(S, Ajax, XTemplate) {
         },
         bind: function() {
             var self = this;
-            self.tap();
+            self.partyTapHoldEvt();
+            self.sessionTapHoldEvt();
             self.add();
             self.update();
             self.del();
         },
+        /**
+         * party taphold效果
+         */
+        partyTapHoldEvt: function(){
+            
+             E.on('.J_PartyOpts','click tapHold', function(e){
+                var t = e.currentTarget,
+                    partyOpts = D.get('.party-opts', t);
+                D.css(partyOpts, 'visibility', 'visible');
 
+                E.on(document, 'click', function(e){
+                    if(!t.contains(e.target) && !partyOpts.contains(e.target) )
+                        D.css(partyOpts, 'visibility', 'hidden');
+                });
+            });   
+        
+        },
 
         /**
-         * tap效果
+         * session taphold 效果
          */
-        tap: function(){
+        sessionTapHoldEvt: function(){
+            
+               E.delegate('#session-list','click tapHold', '.J_SessionOpts', function(e){
+                    var t = e.currentTarget,
+                        sessionOpts = D.get('.session-opts', t);
+                    D.css(sessionOpts, 'visibility', 'visible');
 
+                    E.on(document, 'click', function(e){
+                        if(!t.contains(e.target) && !sessionOpts.contains(e.target) )
+                            D.css(sessionOpts, 'visibility', 'hidden');
+                    });
+                }); 
+            
         },
 
 
