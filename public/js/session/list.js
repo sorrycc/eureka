125f03e10bf81506d3549b3497919577bfa50318
shuier <zhudan000@gmail.com>
2013-04-24 17:42:18 +0800

    add code rotate effect

--- public/js/session/list.js
+++ public/js/session/list.js
@@ -22,7 +22,7 @@ KISSY.add("session/list", function(S, Ajax, XTemplate) {
         _init: function() {
             if (!this.el || !this.tpl) return;
 
-            this.id = this.el.attr("data-id");
+            this.id = this.el.attr('data-party-id');
             this.bind();
         },
         render: function() {
@@ -52,9 +52,7 @@ KISSY.add("session/list", function(S, Ajax, XTemplate) {
             var self = this;
             self.partyTapHoldEvt();
             self.sessionTapHoldEvt();
-            self.add();
-            self.update();
-            self.del();
+            self.viewOriginalCodeEvt();
         },
         /**
          * party taphold效果
@@ -62,6 +60,9 @@ KISSY.add("session/list", function(S, Ajax, XTemplate) {
         partyTapHoldEvt: function(){
             
              E.on('.J_PartyOpts','click tapHold', function(e){
+
+                if(D.hasClass(e.target,'icon-code')) return;
+
                 var t = e.currentTarget,
                     partyOpts = D.get('.party-opts', t);
                 D.css(partyOpts, 'visibility', 'visible');
@@ -79,45 +80,77 @@ KISSY.add("session/list", function(S, Ajax, XTemplate) {
          */
         sessionTapHoldEvt: function(){
             
-               E.delegate('#session-list','click tapHold', '.J_SessionOpts', function(e){
-                    var t = e.currentTarget,
-                        sessionOpts = D.get('.session-opts', t);
-                    D.css(sessionOpts, 'visibility', 'visible');
+           E.delegate('#session-list','click tapHold', '.J_SessionOpts', function(e){
+                var t = e.currentTarget,
+                    sessionOpts = D.get('.session-opts', t);
+                D.css(sessionOpts, 'visibility', 'visible');
 
-                    E.on(document, 'click', function(e){
-                        if(!t.contains(e.target) && !sessionOpts.contains(e.target) )
-                            D.css(sessionOpts, 'visibility', 'hidden');
-                    });
-                }); 
+                E.on(document, 'click', function(e){
+                    if(!t.contains(e.target) && !sessionOpts.contains(e.target) )
+                        D.css(sessionOpts, 'visibility', 'hidden');
+                });
+            }); 
             
         },
 
-
         /**
-         * 新增
+         * 查看二维码原图
+         * 通过动画transform实现Y轴上的旋转
          */
-        add: function(){
+        viewOriginalCodeEvt: function(){
+            var self = this,
+                rotateEl = D.get('#session-list-page');
+            E.on('#J_ViewOriginal', 'click', function(e){
+                // hide session-list-wrap, and display code
+                rotateYDIV(0, rotateEl);
+            });
+            E.on('#code','click', function(){
+                rotateYDIV(180, rotateEl);
+            })
 
-        },
-
-        /**
-         * 编辑
-         */
-        update: function(){
-
-        },
-
-        /**
-         * 删除
-         */
-        del: function(){
-        
         }
 
         
 
 
     });
+
+    function rotateYDIV(ny, el)
+    {
+        var rotYINT;
+
+        if(rotYINT)
+            clearInterval(rotYINT);
+
+        rotYINT = setInterval(function(){
+
+            ny = ny + 1
+            D.css(el, 'transform', 'rotateY(' + ny + 'deg)');
+            D.css(el, 'webkitTransform', 'rotateY(' + ny + 'deg)');
+            D.css(el, 'OTransform', 'rotateY(" + ny + "deg)');
+            D.css(el, 'MozTransform', 'rotateY(" + ny + "deg)');
+            // el.style.transform = 
+            // el.style.webkitTransform = "rotateY(" + ny + "deg)"
+            // el.style.OTransform = "rotateY(" + ny + "deg)"
+            // el.style.MozTransform = "rotateY(" + ny + "deg)"
+            if (ny == 180 || ny >= 360)
+            {
+            clearInterval(rotYINT)
+            if (ny >= 360){ny = 0}
+            }
+            // hide session-list-wrap, and display code
+            if(ny == 90){
+                D.hide('#session-list-wrap')
+                D.show('#code');
+            }
+            if(ny == 270){
+                D.show('#session-list-wrap')
+                D.hide('#code');
+            }
+
+        },10);
+    }
+    
 
     return List;
 
