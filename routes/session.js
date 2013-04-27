32e682ae68b00d6701267322723bb7187465b138
shuier <zhudan000@gmail.com>
2013-04-27 17:14:17 +0800

    change reset.css and edit common header css

--- routes/session.js
+++ routes/session.js
@@ -153,7 +153,7 @@ exports.detail = function(req, res) {
 exports.list = {
   render: function(req, res){
     res.render('session/list', { 
-        title: '分享列表',
+        docTitle: '分享会',
         headAdd: true,
         id: req.params.id || ""
       }); 
