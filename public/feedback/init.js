72ceef8e60d0c83399306f5bad5bf62b7bd63778
minghe <minghe36@126.com>
2013-05-21 21:22:43 +0800

    fix bug

--- public/feedback/init.js
+++ public/feedback/init.js
@@ -30,6 +30,10 @@ KISSY.add(function(S, Node,Uri,Count,CountImage,saveCount,sessionList) {
             //触发统计
             count.count(starNum);
             isExistFeedback = true;
+
+            var num = count.get('value');
+            var people = count.get('time');
+            saveCount(num,people);
         });
 
         /**
@@ -46,17 +50,12 @@ KISSY.add(function(S, Node,Uri,Count,CountImage,saveCount,sessionList) {
                     var isExceed = t >= 2*60*1000;
                     if(isExceed){
                         var num = count.get('value');
-                        var people = count.get('time');
                         //星数
                         var starNum = count.get('average');
                         if(starNum>0){
                             countImage.show(function(){
                                 countImage.set('num',starNum);
                             })
-                        }
-                        //存在用户反馈，更新下统计数据
-                        if(isExistFeedback){
-                            saveCount(num,people);
                         }
                         //清理定时轮询
                         clearInterval(timer);
